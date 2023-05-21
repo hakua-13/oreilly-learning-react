cd9b907e46f92d8925015f495e421f7d8e762d7c
hakua <hakua1452@gmail.com>
2023-05-21 23:39:06 +0900

    カスタムフック

--- chapter-06-state/01/src/App.js
+++ chapter-06-state/01/src/App.js
@@ -1,25 +1,44 @@
 import { useState } from 'react';
+import { v4 } from 'uuid';
 
 import { ColorList } from './ColorList';
 import ColorData from './color-data.json';
+import { AddColorForm } from './AddColorForm';
 
 function App() {
   const [colors, setColors] = useState(ColorData);
 
   return (
-    <ColorList
-      colors={colors}
-      onRateColor={(id, rating) => {
-        const newColors = colors.map(color =>
-          color.id === id ? {...color, rating}: color
-        );
-        setColors(newColors);
-      }}
-      onRemoveColor={(id) => {
-        const newColors = colors.filter(color => color.id !== id);
-        setColors(newColors);
-      }}
-    />
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color,
+
+            }
+          ];
+          setColors(newColors);
+        }}
+      />
+      <ColorList
+        colors={colors}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map(color =>
+            color.id === id ? {...color, rating}: color
+          );
+          setColors(newColors);
+        }}
+        onRemoveColor={(id) => {
+          const newColors = colors.filter(color => color.id !== id);
+          setColors(newColors);
+        }}
+      />
+    </>
   );
 }
 
