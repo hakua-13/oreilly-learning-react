578f6d48d8adc12def76e5e4d744e910d674b9a7
hakua <hakua1452@gmail.com>
2023-05-23 08:01:56 +0900

    context useStateとの併用(データの変更が可能になる)

---
+++ chapter-06-state/01/src/ColorProvider.js
@@ -0,0 +1,41 @@
+import { createContext, useState } from 'react';
+import { v4 } from 'uuid';
+
+import colorData from './color-data.json';
+
+
+export const ColorContext = createContext();
+
+export const ColorProvider = ({ children }) => {
+  const [colors, setColors] = useState(colorData);
+  
+  const addColor = ( title, color ) => {
+    const newColors = [
+      ...colors,
+      {
+        id: v4(),
+        title,
+        color,
+        rating: 0,
+      }
+    ];
+    setColors(newColors);
+  };
+
+  const removeColor = (id) => {
+    const newColors = colors.filter((color) => color.id !== id);
+    setColors(newColors);
+  }
+
+  const rateColor = (id, rating) => {
+    const newColors = colors.map((color) => color.id === id? {...color, rating}: color);
+    setColors(newColors);
+  }
+
+  // setColorsをコンテキストに入れてしまうとどんなそうでもできてしまうので、将来バグが混入される可能性が高くなる。
+  return(
+    <ColorContext.Provider value={{ colors, addColor, removeColor, rateColor }}>
+      {children}
+    </ColorContext.Provider>
+  )
+}
